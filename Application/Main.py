21f082c9af50f5521ec4d8f6b81f4f8cf7f06820
Suradit Luo <suradit_luo@cmu.ac.th>
2024-09-21 15:11:19 +0700

    fetch data

--- Application/Main.py
+++ Application/Main.py
@@ -4,16 +4,35 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 import json
+import datetime
 load_dotenv()
 database_id = os.getenv("NOTION_DATABASE_ID")
+task_id = os.getenv("NOTION_TASK_DATABASE_ID")
 notion_api = os.getenv("NOTION_KEY")
+
 headers = {
     "Authorization": "Bearer " + notion_api,
     "Content-Type": "application/json",
     "Notion-Version": "2022-06-28",
 }
+
+def extract_task_info(data):
+    info = {'id': None, 'task_name': "", 'status': None, 'due_date': datetime}
+
+    properties = data.get("properties", {})
+    for key, value in properties.items():
+        if key == "ID":
+            info["id"] = f"{value["unique_id"]["prefix"]}-{value["unique_id"]["number"]}"
+        elif key == "Task Name":
+            info["task_name"] = value["title"][0]["plain_text"]
+        elif key == "Status":
+            info["status"] = value["status"]["name"]
+        elif key == "Due Date":
+            info["due_date"] = datetime.datetime.strptime(value["date"]["start"],"%Y-%m-%d").timestamp
+    return info if info else None
+
 def get_database():
-    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+    url = f"https://api.notion.com/v1/databases/{task_id}/query"
     page_size = 100
 
     payload = {"page_size": page_size}
@@ -25,5 +44,9 @@ def get_database():
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
     print(results)
+    results = [extract_task_info(item) for item in data["results"]]
+    for result in results:
+        print(result)
+
 if __name__ == '__main__':
     get_database()
