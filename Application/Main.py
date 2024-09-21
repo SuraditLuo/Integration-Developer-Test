082187df69e2bd4b7179a6236852fe893a96baf8
Suradit Luo <suradit_luo@cmu.ac.th>
2024-09-21 17:47:50 +0700

    fix timestamp format

--- Application/Main.py
+++ Application/Main.py
@@ -5,11 +5,13 @@ import os
 import pandas as pd
 import json
 import datetime
+import psycopg2
 load_dotenv()
 database_id = os.getenv("NOTION_DATABASE_ID")
 task_id = os.getenv("NOTION_TASK_DATABASE_ID")
 notion_api = os.getenv("NOTION_KEY")
-
+postgres_port = os.getenv("POSTGRES_PORT")
+psql_password = os.getenv("POSTGRES_PASSWORD")
 headers = {
     "Authorization": "Bearer " + notion_api,
     "Content-Type": "application/json",
@@ -28,8 +30,28 @@ def extract_task_info(data):
         elif key == "Status":
             info["status"] = value["status"]["name"]
         elif key == "Due Date":
-            info["due_date"] = datetime.datetime.strptime(value["date"]["start"],"%Y-%m-%d").timestamp
+            timestamp = datetime.datetime.strptime(value["date"]["start"],"%Y-%m-%dT%H:%M:%S.%f%z")
+            info["due_date"] = timestamp.strftime("%Y-%m-%d %H:%M:%S:%z")
     return info if info else None
+
+def insert_into_database(task):
+    sql = """
+        INSERT INTO task (id, task_name, status, due_date)
+        VALUES (%s, %s, %s, %s);
+    """
+    values = [task['id'], task['task_name'], task['status'], task['due_date']]
+    conn = psycopg2.connect(
+        dbname="notiondb",
+        user="postgres",
+        password=psql_password,
+        host="localhost",
+        port=postgres_port 
+    )
+    cursor = conn.cursor()
+    cursor.execute(sql, values)
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 def get_database():
     url = f"https://api.notion.com/v1/databases/{task_id}/query"
@@ -43,9 +65,9 @@ def get_database():
     filename = "output.json"
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
-    print(results)
     results = [extract_task_info(item) for item in data["results"]]
     for result in results:
+        # insert_into_database(result)
         print(result)
 
 if __name__ == '__main__':
